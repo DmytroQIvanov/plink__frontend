c118143e91dd72b646d16815a78432df40f08767
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2021-07-22 09:18:46 +0300

    chenged hostAddress path

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const hostAddress ="http://localhost:8080"
-// export const hostAddress ="http://localhost:3000"
+// export const hostAddress ="http://localhost:8080"
+export const hostAddress ="https://plink-backend333.herokuapp.com"
